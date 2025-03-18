ce93800266f450b360bb20ce86e6e69c988a5396
Aman Kumar Verma <116552372+18-05-Amankumarverma@users.noreply.github.com>
2025-03-19 01:34:22 +0530

    add vercel config

--- healthcare/healthcare/settings.py
+++ healthcare/healthcare/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-33d9-bnv&btq^uk(4agz$zko$exf0#9&phxr&__kvp7y)+yn24
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app",".now.sh"]
 
 
 
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfiles_build','static')]
 
 MEDIA_URL = 'media/'
 MEDIA_DIRS = os.path.join(BASE_DIR,'media')
